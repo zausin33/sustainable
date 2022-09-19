078bbb7076140ab4e3d8b6a85f2a4a1f9ca5c969
Jonas Zausinger <jonas.zausinger@iamds.com>
2022-09-19 18:00:38 +0200

    first commit

---
+++ sustainable-api/src/main/java/com/sustainable/sustainableapi/SustainableApiApplication.java
@@ -0,0 +1,13 @@
+package com.sustainable.sustainableapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustainableApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SustainableApiApplication.class, args);
+    }
+
+}
---
+++ sustainable-api/src/test/java/com/sustainable/sustainableapi/SustainableApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sustainable.sustainableapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustainableApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
